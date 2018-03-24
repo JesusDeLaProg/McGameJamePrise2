8cd1cda647bf8d70dc3631e52d9039101ab353e4
Maxime Charland - CHAM14059707 <maxime.charland.1@ens.etsmtl.ca>
2018-03-24 11:21:18 -0400

    Reimport audio from mcgamejamV1

---
+++ audio/audioclipmanager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class audioclipmanager : MonoBehaviour {
+
+	public Sound[] sounds;
+
+	void Awake() 
+	{
+		foreach (Sound i in sounds) 
+		{
+			i.source = gameObject.AddComponent<AudioSource> ();
+
+			i.source.clip = i.clip;
+			i.source.volume = i.volume;
+			i.source.pitch = i.pitch;
+			i.source.loop = i.loop;
+
+		}
+	
+	}
+
+	public void Play(string name)
+	{
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+
+		if (s != null) {
+		
+			s.source.Play ();
+
+		
+		}
+
+	}
+
+}
