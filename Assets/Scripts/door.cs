16f2142bdf7a69a68f8227366301bf8ee19ff339
Caroline <caropala9@gmail.com>
2018-03-24 17:37:28 -0400

    ascenceur

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class door : MonoBehaviour
+{
+
+    public Transform point;
+    public PlayableDirector pd;
+
+    public PlayableDirector pd2;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown("w"))
+            {
+                pd.Play();
+                pd2.Play();
+                other.gameObject.transform.position = point.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/elevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class elevator : MonoBehaviour {
+
+    public Transform point;
+    public PlayableDirector pd;
+
+    public PlayableDirector pd2;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown("w"))
+            {
+                pd.Play();
+                pd2.Play();
+                other.gameObject.transform.position = point.position;
+            }
+        }
+    }
+}
