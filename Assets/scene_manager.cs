e05bc909a7dea045494141a784b07374548e2f02
Caroline <caropala9@gmail.com>
2018-03-25 13:20:32 -0400

    game loop is done

---
+++ Assets/scene_manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene_manager : MonoBehaviour {
+
+    public GameObject countdown;
+    public string levelToLoad;
+
+	
+	
+	// Update is called once per frame
+	void GameOver () {
+		
+
+
+	}
+}
