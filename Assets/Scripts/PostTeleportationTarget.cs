f619bdaa4eab30319a0a7f0dd9f6353f3f3f0576
JesusDeLaProg <charland.maxime@gmail.com>
2018-03-25 02:34:18 -0400

    Create RotationModule Create Move TeleportationModule Implement building rotation after teleporting the player

---
+++ Assets/Scripts/PostTeleportationTarget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostTeleportationTarget : MonoBehaviour {
+
+    private RotationModule _rotationModule;
+    public Vector3 PlaneRotation;
+
+	// Use this for initialization
+	void Start () {
+        _rotationModule = GameObject.FindGameObjectWithTag("Building").GetComponent<RotationModule>();
+	}
+
+    public bool IsPlayerInside()
+    {
+        var playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
+        var closestPosition = GetComponent<Collider>().ClosestPoint(playerPosition);
+        Debug.Log("Player pos: " + playerPosition + " Closest pos: " + closestPosition);
+        return Math.Abs((closestPosition - playerPosition).magnitude) < 0.1f;
+    }
+
+}
